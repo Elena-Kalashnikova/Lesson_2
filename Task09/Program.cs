e57a66d9cf9ed892847ce464976ff7bfaee31ef3
Елена <lenna@gmail.com>
2023-08-12 21:25:44 +0300

    Add program in Task09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 9. Напишите программу, которая выводит
+// случайное число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+using System.Globalization;
+
+int number = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из диапозона 10 - 99 -> {number}");
+int  maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра = {maxDigit}");
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+// if (firstDigit > secondDigit)
+// {
+//     Console.WriteLine($"Наибольшая цифра = {firstDigit}");
+// }
+// else
+// {
+//     Console.WriteLine($"Наибольшая цифра = {secondDigit}");
+// }
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    // if (firstDigit > secondDigit)
+    // {
+    //     return firstDigit;
+    // }
+    return firstDigit > secondDigit ? firstDigit: secondDigit;
+}
