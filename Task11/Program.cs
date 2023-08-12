116fb6257e564d3cd8dea31a7cf4db72c19649fb
Елена <lenna@gmail.com>
2023-08-12 21:54:55 +0300

    Add program in Task11

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трехзначное число из диапозона 100 - 999 ->{number}");
+int resultDigit = DeleteSecondDigit(number);
+Console.WriteLine($"Число без второй цифры = {resultDigit}");
+
+int DeleteSecondDigit(int num){
+    int firstDigit = num / 100;
+    int lastDigit = num % 10;
+    int resDigit = firstDigit * 10 + lastDigit;
+    return resDigit;
+  
+
+}
