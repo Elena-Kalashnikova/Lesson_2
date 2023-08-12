ee6052b9619a5a2035bae67331509065a31a0b88
Елена <lenna@gmail.com>
2023-08-12 22:37:44 +0300

    Add program in Task14

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+
+
+using System.Reflection.Metadata.Ecma335;
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+bool result = Multiplicity(number);
+Console.WriteLine(result ? "Да" : "Нет");
+
+
+// int Multiplicity_7_23(int num){
+//   return num % 7 % 23;
+// }
+bool Multiplicity (int num){
+    return num % 7 == 0 && num % 23 == 0;
+}
