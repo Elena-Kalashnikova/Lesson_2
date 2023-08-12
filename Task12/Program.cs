1ddf5023711792aa8ff92fd3831abb71ebe4bb4a
Елена <lenna@gmail.com>
2023-08-12 22:11:09 +0300

    Add program in Task12

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 12. Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли первое число
+// кратным второму. Если первое число не кратно
+// второму, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.WriteLine("Введите первое число:");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+int result = IsMultiplicity(number_1, number_2);
+if (result == 0)
+{
+
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine($"Некратно, остаток = {result}");
+}
+
+int IsMultiplicity(int num_1, int num_2)
+{
+    return num_1 % num_2;
+}
+
+
